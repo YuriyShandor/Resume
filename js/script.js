6f6e1cf53d285ff8a9ee2bcab192ade344ba2466
Yuriy Shandor <yuriyshandor@gmail.com>
2017-12-29 15:23:35 +0200

    Add Section About Me. Mod_01

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ $(document).ready(function () {
     $('.nav-bar').slideToggle('0.5s');
   });
 });
+
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+document.getElementById('my-age').innerHTML = getAge("1993/05/23");
