2813772597a61b82dccc8eb3e55b4b7f47b1ba81
Yuriy Shandor <yuriyshandor@gmail.com>
2018-04-29 11:29:55 +0300

    Add Improve Mod_04

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 
   $('.portfolio-item__inside').hoverDirection();
 
+  // Section Navigation
   $('a[href^="#"]').on('click',function (e) {
 	  e.preventDefault();
 
@@ -18,17 +19,29 @@ $(document).ready(function () {
 	      window.location.hash = target;
 	  });
 	});
-});
 
-function getAge(dateString) {
-  var today = new Date();
-  var birthDate = new Date(dateString);
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
+  // Script for Birth Date
+  function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
   }
-  return age;
-}
 
-document.getElementById('my-age').innerHTML = getAge("1993/05/23");
+  $('#my-age').html(getAge("1993/05/23"));
+
+  // Script for Experience Count
+  var experienceStart = Date.parse("2018/03/01");
+  var todayDate = new Date();
+  var diffDate =  todayDate - experienceStart;
+
+  var experienceYears = Math.floor(diffDate / 31536000000);
+  var experienceMonths = Math.floor((diffDate % 31536000000) / 2628000000);
+
+  $('#experienceYears').html(experienceYears);
+  $('#experienceMonths').html(experienceMonths);
+});
